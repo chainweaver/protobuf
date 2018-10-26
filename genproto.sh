95f968eac0d0110c0a3036dbd3a7eb86ce92446e
betchi <shinichi.minobe@gmail.com>
2018-10-26 17:33:04 +0900

    Remove gogoproto

--- genproto.sh
+++ genproto.sh
@@ -6,7 +6,7 @@ rm -f ./protoc-gen-js/*
 protoc \
   -I./proto \
   -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --gofast_out=plugins=grpc:../../../ \
+  --go_out=plugins=grpc:../../../ \
   --grpc-gateway_out=logtostderr=true:../../../ \
   --js_out=import_style=commonjs,binary:./protoc-gen-js/ \
   --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:./protoc-gen-js/ \
