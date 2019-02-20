fe928b323b8071d1f8554314e764df1f6a26400e
betchi <shinichi.minobe@gmail.com>
2019-02-20 16:29:12 +0900

    Update renderOpenApiDoc.sh

--- scripts/renderOpenApiDoc.sh
+++ scripts/renderOpenApiDoc.sh
@@ -6,12 +6,14 @@ node /api2html/bin/api2html.js \
   --out openapi/btc.html \
   --languages shell,go,javascript,javascript--nodejs,ruby,python,java \
   --customLogo logo-white.png \
-  openapi/btc/openapi.yaml
+  openapi/btc/openapi3.yaml
 
+mkdir -p pub/css
+touch pub/css/theme_override.css
 node /api2html/bin/api2html.js \
   --out openapi/eth.html \
   --languages shell,go,javascript,javascript--nodejs,ruby,python,java \
   --customLogo logo-white.png \
-  openapi/eth/openapi.yaml
+  openapi/eth/openapi3.yaml
 
 rm -f logo-white.png
