810c9952866b9d0e3efd816319b55ed5dbd51885
betchi <shinichi.minobe@gmail.com>
2019-03-04 11:39:41 +0900

    Add {address} test

---
+++ postman/eth/testScript/address_balance_endpoint.js
@@ -0,0 +1,20 @@
+let testCaseNo = pm.variables.get("test_case_no");
+let res = pm.response.json();
+switch (testCaseNo) {
+  case "test-address-0001":
+    tests["Status code is 200"] = responseCode.code === 200;
+    eval(globals.commonTests)("ethAddress");
+    break;
+  case "test-address-0002":
+    tests["Status code is 200"] = responseCode.code === 200;
+    eval(globals.commonTests)("ethAddress");
+    break;
+  case "test-address-0003":
+    tests["Status code is 404"] = responseCode.code === 404;
+    eval(globals.commonTests)("error");
+    tests["Error message is expected"] = res.error === "Wallet not-exist-address not found";
+    break;
+  default:
+    tests["Status code is 200"] = responseCode.code === 200;
+    eval(globals.commonTests)("ethAddress");
+}
