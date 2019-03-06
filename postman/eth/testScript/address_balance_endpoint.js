da85135d763c7f727b16655f81f768062b2ef5b2
betchi <shinichi.minobe@gmail.com>
2019-03-06 14:51:21 +0900

    Add test to address

--- postman/eth/testScript/address_balance_endpoint.js
+++ postman/eth/testScript/address_balance_endpoint.js
@@ -1,20 +1,58 @@
 let testCaseNo = pm.variables.get("test_case_no");
 let res = pm.response.json();
+
 switch (testCaseNo) {
   case "test-address-0001":
-    tests["Status code is 200"] = responseCode.code === 200;
+    eval(globals.assert)("http status code", 200, responseCode.code);
     eval(globals.commonTests)("ethAddress");
+    eval(globals.assert)("address is expected", "b7695173b4aed4f392361eca3ab19104ba1c0b13", res.address);
+    eval(globals.assert)("total_received is expected", 10000000000000000, res.total_received);
+    eval(globals.assert)("total_sent is expected", 0, res.total_sent);
+    eval(globals.assert)("balance is expected", 10000000000000000, res.balance);
+    eval(globals.assert)("unconfirmed_balance is expected", 0, res.unconfirmed_balance);
+    eval(globals.assert)("final_balance is expected", 10000000000000000, res.final_balance);
+    eval(globals.assert)("n_tx is expected", 1, res.n_tx);
+    eval(globals.assert)("unconfirmed_n_tx is expected", 0, res.unconfirmed_n_tx);
+    eval(globals.assert)("final_n_tx is expected", 1, res.final_n_tx);
+    eval(globals.assert)("response object property count", 9, Object.keys(res).length);
     break;
   case "test-address-0002":
-    tests["Status code is 200"] = responseCode.code === 200;
+    eval(globals.assert)("http status code", 200, responseCode.code);
     eval(globals.commonTests)("ethAddress");
+    eval(globals.assert)("address", "0000000000000000000000000000000000000000", res.address);
+    eval(globals.assert)("response object property count", 9, Object.keys(res).length);
     break;
   case "test-address-0003":
-    tests["Status code is 404"] = responseCode.code === 404;
+    eval(globals.assert)("http status code", 200, responseCode.code);
+    eval(globals.commonTests)("ethAddress");
+    eval(globals.assert)("address", "0000000000000000000000000000000000000000", res.address);
+    eval(globals.assert)("response object property count", 9, Object.keys(res).length);
+    break;
+  case "test-address-0004":
+    eval(globals.assert)("http status code", 200, responseCode.code);
+    eval(globals.commonTests)("ethAddress");
+    eval(globals.assert)("address", "0000000000000000000000000000000000000000", res.address);
+    eval(globals.assert)("response object property count", 9, Object.keys(res).length);
+    break;
+  case "test-address-0005":
+    break;
+  case "test-address-0006":
+    break;
+  case "test-address-0007":
+    break;
+  case "test-address-0008":
+    break;
+  case "test-address-0009":
+    break;
+  case "test-address-0010":
+    break;
+  case "test-address-0011":
+    eval(globals.assert)("http status code", 404, responseCode.code);
     eval(globals.commonTests)("error");
-    tests["Error message is expected"] = res.error === "Wallet not-exist-address not found";
+    eval(globals.assert)("error message", "Wallet not-exist-address not found", res.error);
+    eval(globals.assert)("response object property count", 1, Object.keys(res).length);
     break;
   default:
-    tests["Status code is 200"] = responseCode.code === 200;
+    eval(globals.assert)("http status code", 200, responseCode.code);
     eval(globals.commonTests)("ethAddress");
 }
